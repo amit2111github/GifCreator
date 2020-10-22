604b44dec9d95850e18e8944ce9898332fea64e9
Amit Kumar <72341567+amitmandal2111@users.noreply.github.com>
2020-10-22 18:31:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+ function getgif(){
+
+
+ 	fetch("https://api.giphy.com/v1/gifs/search?q=hilarious&api_key=dc6zaTOxFJmzC")
+ 	.then((res) => res.json())
+ 	.then((val) =>{
+ 		var output = ""
+ 		for(var i = 0 ; i<3; i++){
+ 			var  number  = parseInt(Math.random()*50)
+ 			output += `
+ 				<img src ="${val.data[number].images.original.url} width= "300px" height= "200px">
+ 			`
+ 		}
+ 		document.querySelector(".insert").innerHTML = output
+
+ 			
+ 	})
+
+}
+
+document.querySelector(".btn").addEventListener("click",getgif)
